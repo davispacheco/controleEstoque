6d7fcf2cd4f470f2ccce60c6863059951dbc06e5
davispacheco <davipacheco1996@gmail.com>
2022-09-09 11:30:46 -0300

    Corrigir mais conflitos de branch.

--- src/main/java/com/modulo5/controleEstoque/model/ClienteModel.java
+++ src/main/java/com/modulo5/controleEstoque/model/ClienteModel.java
@@ -36,9 +36,9 @@ public class ClienteModel implements Serializable {
 
     @JsonIgnore
     @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
-<<<<<<< HEAD
     private List<EnderecoModel> enderecos = new ArrayList<>();
-=======
-    private List<PedidoModel> pedidos;
+
+    @OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
+    private List<PedidoModel> pedidos = new ArrayList<>();
 >>>>>>> fcfa20f8452ced0c43aa540db552a9acce171df6
 }
